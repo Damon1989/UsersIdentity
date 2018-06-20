473dba7780fff41e2c8d648e04ee669115890a2f
杨小东 <1015080921@qq.com>
2018-06-20 17:45:18 +0800

    roleadmin edit 修改

--- UsersIdentity/Controllers/RoleAdminController.cs
+++ UsersIdentity/Controllers/RoleAdminController.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -44,7 +45,46 @@ namespace UsersIdentity.Controllers
         public async Task<ActionResult> Edit(string id)
         {
             var role = await RoleManager.FindByIdAsync(id);
-            return View();
+            var memberIds = role.Users.Select(x => x.UserId).ToArray();
+            var members = UserManager.Users.Where(x => memberIds.Any(y => y == x.Id));
+            var nonMembers = UserManager.Users.Except(members);
+
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(RoleModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (var userId in model.IdsToAdd ?? new string[] { })
+                {
+                    result = await UserManager.AddToRoleAsync(userId, model.RoleName);
+                    if (!result.Succeeded)
+                    {
+                        return View("Error", result.Errors);
+                    }
+                }
+
+                foreach (var userId in model.IdsToDelete ?? new string[] { })
+                {
+                    result = await UserManager.RemoveFromRoleAsync(userId, model.RoleName);
+                    if (!result.Succeeded)
+                    {
+                        return View("Error", result.Errors);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Error", new string[] { "Role Not Found" });
         }
 
         public async Task<ActionResult> Delete(string id)
